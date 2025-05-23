9f97fa2dca3ec64afbc3e66d4eb033590aa9a92e
Ngohug2003 <ngoviethung0911@gmail.com>
2025-05-23 11:17:41 +0700

    livewire

--- resources/views/Auth/show.blade.php
+++ resources/views/Auth/show.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.client.app')
 
 @section('title', 'Thông tin tài khoản')
 
--- resources/views/client/orders/index.blade.php
+++ resources/views/client/orders/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.client.app')
 
 @section('title', 'Lịch sử đơn hàng')
 
